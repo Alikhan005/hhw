754b91e977e31890726aa2c1e207c86420ddc53e
ALIKHAN SAPAR <saparov.05@mail.ru>
2025-02-25 18:34:03 +0600

    Удалена папка bonus_task

--- bonus_task/note/admin.py
+++
@@ -1,8 +0,0 @@
-# note/admin.py
-from django.contrib import admin
-from .models import Note
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at')
-    search_fields = ('title',)
