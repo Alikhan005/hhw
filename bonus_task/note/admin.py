72103dfdf04fb3634e37abec5b20257ec0f5a43e
Miras489 <Miras489>
2025-03-17 00:04:41 +0600

    bonus

---
+++ bonus_task/note/admin.py
@@ -0,0 +1,8 @@
+# note/admin.py
+from django.contrib import admin
+from .models import Note
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+    search_fields = ('title',)
